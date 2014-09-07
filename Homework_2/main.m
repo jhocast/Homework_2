9a892ed13be71a05ea11587e51f9eea4426d7401
jhocast <jhocast@gmail.com>
2014-09-07 16:17:50 -0700

    Initial Commit

---
+++ Homework_2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Homework_2
+//
+//  Created by Jhon Castrillon on 9/7/14.
+//  Copyright (c) 2014 Jhonny Castrillon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
